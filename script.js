53b143e9eb327ceba80d9b03bbb72814203f1b28
christovanqueiroz <106829027+christovanqueiroz@users.noreply.github.com>
2023-03-03 20:12:19 -0300

    feat: consulta API

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ let mês = date.getMonth();
 let ano = date.getFullYear();
 
 dataDeHoje.value = `${dia}/${mês+1}/${ano}`
+
+const dataDoTratamento = document.getElementById('tratamento');
+dataDoTratamento.value = `${dia+1}/${mês+1}/${ano}`
+
+let cnpjDoCliente;
+let url;
+let data;
+let cnpj;
+
+const consultaCNPJ = async() => {
+    cnpjDoCliente = document.getElementById('cnpj').value;
+    url = `https://publica.cnpj.ws/cnpj/${cnpjDoCliente}`;
+    data = await fetch(url);
+    cnpj = await data.json();
+    let nomeDoCliente = document.getElementById('cliente');
+    nomeDoCliente.value = cnpj.razao_social
+}
